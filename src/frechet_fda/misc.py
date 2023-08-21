94aed80429d05035a19ba39457050991faa0465c
Mirkan Öcal <mirkan.oecal@fit.fraunhofer.de>
2023-08-21 18:21:49 +0200

    Progress with Riemann sum finally.

--- src/frechet_fda/misc.py
+++ src/frechet_fda/misc.py
@@ -23,3 +23,31 @@ def riemann_sum(a, b, f, method="midpoint", step_size=None):
     values = f(grid) * step_size
     cdf_values = np.cumsum(values)
     return np.interp(b, grid, cdf_values)
+
+
+def riemann_sum_arrays(left_bound, right_bound, multi_dim_array, axis):
+    """"Computes riemann sum for given array, along the axis that contains the grid of
+    values.
+    """
+    m = multi_dim_array.shape[axis]  # Number of points along the axis of grid values
+    step_size = (right_bound - left_bound) / m
+
+    # Compute the Riemann sum along the axis of grid values using vectorized computation
+    return np.sum(multi_dim_array, axis=axis) * step_size
+
+
+def l2_norm(left_bound_support, right_bound_support, array, axis):
+    """Compute L2 norm of (approximate) function."""
+    return np.sqrt(
+        riemann_sum_arrays(
+            left_bound=left_bound_support,
+            right_bound=right_bound_support,
+            multi_dim_array=array**2,
+            axis=axis,
+        ),
+    )
+
+
+# def wasserstein_norm(left_bound_support, right_bound_support, array, axis):
+#     """Compute Wasserstein norm."""
+# To Do!!
