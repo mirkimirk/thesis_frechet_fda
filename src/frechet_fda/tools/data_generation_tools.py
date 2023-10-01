Fixed computation of true_m, and cleaned repo a bit.

src/frechet_fda/tools/data_generation_tools.py
@@ -6,49 +6,51 @@ Petersen and Mueller (2016)'s simulation study.
 import warnings
 
 import numpy as np
-from scipy.stats import norm, truncnorm
-
 from frechet_fda.function_class import Function
 from frechet_fda.tools.kernel_methods import (
-    density_estimator, boundary_corrected_density_estimator
+    boundary_corrected_density_estimator,
+    density_estimator,
 )
 from frechet_fda.tools.numerics_tools import riemann_sum_cumulative
+from scipy.stats import norm, truncnorm
+
+
+def gen_predictor_values_regression(
+    size: int = 100, pred_bounds: tuple = (-1, 1), seed: int = None,
+):
+    """Generates scalar predictor for Fréchet regression."""
+    predictor = np.random.default_rng(seed).uniform(
+        pred_bounds[0], pred_bounds[1], size,
+    )
+    return np.sort(predictor)
 
 
 def gen_params_regression(
-        mu_params : dict,
-        sigma_params : dict,
-        size : int = 100,
-        pred_bounds : tuple = (-1,1),
-        seed : int = None):
+    mu_params: dict, sigma_params: dict, predictor: np.ndarray, seed: int = None,
+):
     """Generates mus and sigmas for conditional distributions in regression context.
-    
+
     This reflects the first simulation scenario of Petersen & Müller (2019). Need to
     pass mu_params and sigma params, which have to contain specific entries.
+
     """
-    predictor = np.random.default_rng(seed).uniform(
-        pred_bounds[0], pred_bounds[1], size
-    )
-    predictor.sort()
     # Generate mus that linearly depend on x
     mus = np.random.default_rng(seed).normal(
-        loc=mu_params["mu0"] + mu_params["beta"] * predictor, scale=mu_params["v1"]
+        loc=mu_params["mu0"] + mu_params["beta"] * predictor, scale=mu_params["v1"],
     )
     # Generate sigmas that linearly depend on x
     sh = (
-        (sigma_params["sigma0"] + sigma_params["gamma"] * predictor) ** 2
-        / sigma_params["v2"]
-    )
-    sc = (
-        sigma_params["v2"]
-        / (sigma_params["sigma0"] + sigma_params["gamma"] * predictor)
+        sigma_params["sigma0"] + sigma_params["gamma"] * predictor
+    ) ** 2 / sigma_params["v2"]
+    sc = sigma_params["v2"] / (
+        sigma_params["sigma0"] + sigma_params["gamma"] * predictor
     )
     sigmas = np.random.default_rng(seed).gamma(shape=sh, scale=sc)
 
-    return predictor, mus, sigmas
+    return mus, sigmas
 
 
-def gen_y_qf(mu : np.ndarray, sigma : np.ndarray, eval_grid : np.ndarray):
+def gen_y_qf(mu: np.ndarray, sigma: np.ndarray, eval_grid: np.ndarray):
     """Generate quantile function of Y_i given X_i."""
     ys = (
         mu[..., np.newaxis]
@@ -58,23 +60,23 @@ def gen_y_qf(mu : np.ndarray, sigma : np.ndarray, eval_grid : np.ndarray):
 
 
 def gen_sample_points_from_qfs(
-        quantile_functions : list[Function],
-        size : int = 100,
-        seed : int = None
-    ):
+    quantile_functions: list[Function], size: int = 100, seed: int = None,
+):
     """Generate a sample of observation points for given distributions.
-    
-    Need to supply quantile functions."""
-    which_quantiles = np.random.default_rng(seed).uniform(
-        0, 1, size
-    )
+
+    Need to supply quantile functions.
+
+    """
+    which_quantiles = np.random.default_rng(seed).uniform(0, 1, size)
     return np.array([qf[which_quantiles] for qf in quantile_functions])
 
 
 def gen_params_scenario_one(num_of_distr: int, seed: int = 28071995) -> tuple:
     """Generate parameters for the density samples and define appropriate grids.
-    
-    This is the first simulation scenario of Petersen & Müller (2016)."""
+
+    This is the first simulation scenario of Petersen & Müller (2016).
+
+    """
     # Draw different sigmas
     log_sigmas = np.random.default_rng(seed=seed).uniform(-1.5, 1.5, num_of_distr)
     mus = np.zeros(num_of_distr)
@@ -85,8 +87,10 @@ def gen_params_scenario_one(num_of_distr: int, seed: int = 28071995) -> tuple:
 
 def gen_params_scenario_two(num_of_distr: int, seed: int = 28071995) -> tuple:
     """Generate parameters for the density samples and define appropriate grids.
-    
-    This is the second simulation scenario of Petersen & Müller (2016)."""
+
+    This is the second simulation scenario of Petersen & Müller (2016).
+
+    """
     # Draw different mus
     mus = np.random.default_rng(seed=seed).uniform(-3, 3, num_of_distr)
     sigmas = np.ones(num_of_distr)
@@ -134,7 +138,7 @@ def make_estimated_pdf(
     kern: str = "epanechnikov",
     grid_size: int = 10000,
     bandwidth: np.ndarray = 0.2,
-    bias_corrected : bool = True
+    bias_corrected: bool = True,
 ) -> list[Function]:
     """Turn sample_points array into Function class objects with their corresponding
     grid.
@@ -147,19 +151,12 @@ def make_estimated_pdf(
     # Check if we're only dealing with one single density
     if bias_corrected:
         return boundary_corrected_density_estimator(
-            x_vals = pdfs_x,
-            sample_of_points = sample_points,
-            h=bandwidth,
-            kernel_type=kern
+            x_vals=pdfs_x, sample_of_points=sample_points, h=bandwidth, kernel_type=kern,
         )
     else:
         return density_estimator(
-            x_vals = pdfs_x,
-            sample_of_points = sample_points,
-            h=bandwidth,
-            kernel_type=kern
+            x_vals=pdfs_x, sample_of_points=sample_points, h=bandwidth, kernel_type=kern,
         )
-
 
 
 # Truncated normal pdf
@@ -197,7 +194,10 @@ def make_truncnorm_pdf(
 
     # Check whether each density integrates to 1
     pdfs_y = _check_and_normalize_density(
-        x_vals=pdf_x, y_vals=pdfs_y, eps=1e-5, warn=warn_irregular_densities,
+        x_vals=pdf_x,
+        y_vals=pdfs_y,
+        eps=1e-5,
+        warn=warn_irregular_densities,
     )
     return [(pdf_x, pdf_y) for pdf_y in pdfs_y]
 
@@ -240,7 +240,10 @@ def _norm_cdf(
 
 
 def _check_and_normalize_density(
-    x_vals: np.ndarray, y_vals: np.ndarray, eps: float = 1e-5, warn: bool = True,
+    x_vals: np.ndarray,
+    y_vals: np.ndarray,
+    eps: float = 1e-5,
+    warn: bool = True,
 ) -> np.ndarray:
     """Checks whether y_vals is an array of valid densities, i.e., whether they
     integrate to one.

src/frechet_fda/tools/frechet_tools.py
@@ -2,22 +2,17 @@
 
 import numpy as np
 import quadprog
-
 from frechet_fda.function_class import Function
-from frechet_fda.tools.function_tools import (
-    mean_func, quantile_distance
-)
+from frechet_fda.tools.function_tools import mean_func, quantile_distance
+
 
 def _empirical_weight_function(
-        x_to_predict : float,
-        x_observed : np.ndarray
-    ) -> np.ndarray:
+    x_to_predict: float, x_observed: np.ndarray,
+) -> np.ndarray:
     """Weight function to compute weighted Fréchet mean."""
     x_observed = np.atleast_2d(x_observed)
     means = np.mean(x_observed, axis=-1)
-    cov_matrix = (
-        x_observed @ x_observed.transpose() / x_observed.shape[-1]
-    )
+    cov_matrix = x_observed @ x_observed.transpose() / x_observed.shape[-1]
     inv_cov_matrix = np.linalg.inv(cov_matrix)
     return 1 + (
         (x_observed - means[..., np.newaxis]).transpose()
@@ -27,23 +22,20 @@ def _empirical_weight_function(
 
 
 def qf_tilde(
-        x_to_predict : float,
-        x_observed : np.ndarray,
-        qfs_observed : list[Function]
-    ) -> Function:
+    x_to_predict: float, x_observed: np.ndarray, qfs_observed: list[Function],
+) -> Function:
     """Estimator for quantile function."""
     return mean_func(
-        _empirical_weight_function(x_to_predict, x_observed) * qfs_observed
+        _empirical_weight_function(x_to_predict, x_observed) * qfs_observed,
     )
 
 
 def solve_frechet_qp(
-        xs_to_predict : np.ndarray,
-        x_observed : np.ndarray,
-        quantile_functions : list[Function]
-    ) -> list[Function]:
-    """Sets up quadratic programming problem and solves it.
-    """
+    xs_to_predict: np.ndarray,
+    x_observed: np.ndarray,
+    quantile_functions: list[Function],
+) -> list[Function]:
+    """Sets up quadratic programming problem and solves it."""
     estimates = []
     for x in xs_to_predict:
         # Estimate condtional qf, drop support where values become nan or inf
@@ -57,9 +49,8 @@ def solve_frechet_qp(
             qp_a = estimated_qf.y
             grid_size = len(qp_a)
             qp_g = np.identity(grid_size)  # make sure P is symmetric
-            qp_c = (
-                np.eye(grid_size, grid_size - 1, k=-1)
-                - np.eye(grid_size, grid_size - 1)
+            qp_c = np.eye(grid_size, grid_size - 1, k=-1) - np.eye(
+                grid_size, grid_size - 1,
             )
             qp_b = np.zeros(grid_size - 1)
             solution = quadprog.solve_qp(qp_g, qp_a, qp_c, qp_b)[0]
@@ -68,14 +59,14 @@ def solve_frechet_qp(
 
 
 def ise_wasserstein(
-        m_hat : list[Function],
-        true_m : list[Function],
-        x_to_predict : np.ndarray,
-        already_qf : bool = False
-    ) -> float:
+    m_hat: list[Function],
+    true_m: list[Function],
+    x_to_predict: np.ndarray,
+    already_qf: bool = False,
+) -> float:
     """Compute integrated squared error."""
     distances = [
         quantile_distance(hat, true, already_qf=already_qf)
         for hat, true in zip(m_hat, true_m, strict=True)
     ]
-    return Function(x_to_predict, distances).integrate().y[-1]
+    return Function(x_to_predict, distances).integrate(method="midpoint").y[-1]

src/frechet_fda/tools/function_tools.py
@@ -1,7 +1,6 @@
 """This module contains tools to use with the distribution class."""
 
 import numpy as np
-
 from frechet_fda.function_class import Function
 
 
@@ -12,7 +11,7 @@ def make_function_objects(xy_tuples: list[tuple]) -> list[Function]:
     return [Function(*xy_tuple) for xy_tuple in xy_tuples]
 
 
-def pdf_to_qdf(pdf: Function, save_support_start : bool = False) -> Function:
+def pdf_to_qdf(pdf: Function, save_support_start: bool = False) -> Function:
     """Directly convert a pdf to a qdf using inverse function rule on qf."""
     quantile_func = pdf.integrate().invert()
     if save_support_start:
@@ -22,8 +21,8 @@ def pdf_to_qdf(pdf: Function, save_support_start : bool = False) -> Function:
 
 
 def qdf_to_pdf(
-        qdf: Function, start_val : float = 0, center_on_zero : bool = False
-    ) -> Function:
+    qdf: Function, start_val: float = 0, center_on_zero: bool = False,
+) -> Function:
     """Directly convert a qdf to a pdf using inverse function rule on cdf."""
     if center_on_zero:
         cdf = qdf.integrate().vcenter().invert()
@@ -37,7 +36,7 @@ def get_optimal_range(funcs: list[Function], delta: float = 1e-3) -> np.ndarray:
 
     This is used so the qdfs dont get astronomically large at the boundaries and destroy
     numerical methods.
-    
+
     Note: The method here assumes that the functions do have a compact
     support (even if it is narrower than the initial support). So if there is a point x1
     where func.y > delta is true, another point x2 > x1 where it is not, and then
@@ -67,9 +66,8 @@ def mean_func_legacy(funcs: list[Function]) -> Function:
 
 
 def log_qd_transform(
-        densities_sample: list[Function],
-        different_supports : bool = False
-    ) -> list[Function]:
+    densities_sample: list[Function], different_supports: bool = False,
+) -> list[Function]:
     """Perfrom log quantile density transformation on a density sample."""
     qdfs = [
         pdf_to_qdf(density.drop_inf(), different_supports)
@@ -78,14 +76,13 @@ def log_qd_transform(
     if different_supports:
         qdfs = np.array(qdfs)
         lqdfs = [qdf.log() for qdf in qdfs[:, 0]]
-        return np.array((lqdfs, qdfs[:, 1]))
+        return lqdfs, qdfs[:, 1]
     else:
         return [qdf.log() for qdf in qdfs]
 
 
 def inverse_log_qd_transform(
-    transformed_funcs : list[Function],
-    start_of_support : list[float] = None
+    transformed_funcs: list[Function], start_of_support: list[float] = None,
 ) -> list[Function]:
     """Transform back into density space."""
     natural_qfs = [func.exp().integrate() for func in transformed_funcs]
@@ -101,12 +98,14 @@ def inverse_log_qd_transform(
 
 
 def inverse_log_qd_transform_corrected(
-    transformed_funcs : list[Function],
+    transformed_funcs: list[Function],
 ) -> list[Function]:
     """Invert the log quantile density transform to get back into density space.
-    
+
     Not used, I must understand something wrong about the implementation with a
-    different support."""
+    different support.
+
+    """
     # First compute quantile function via natural inverse
     natural_qfs = [func.exp().integrate() for func in transformed_funcs]
     # Compute correction factors to normalize quantiles
@@ -125,27 +124,28 @@ def inverse_log_qd_transform_corrected(
 
 
 def frechet_mean(
-        density_sample: list[Function], centered_on_zero : bool = False
-    ) -> Function:
+    density_sample: list[Function], centered_on_zero: bool = False,
+) -> Function:
     """Compute Fréchet mean of a given sample of densities.
-    
+
     By default, estimates new support by taking the mean of the left bounds of the
     sample of densities. If centered_on_zero specified, then resulting mean density is
     transformed to be centered around zero.
+
     """
     qdfs_and_start_vals = np.array(
-        [pdf_to_qdf(density, True) for density in density_sample]
+        [pdf_to_qdf(density, True) for density in density_sample],
     )
     qdfs = qdfs_and_start_vals[:, 0]
     start_vals = qdfs_and_start_vals[:, 1]
     mean_qdf = mean_func(qdfs)
-    mean_start_val = np.mean(start_vals) # estimated start of the support of mean pdf
+    mean_start_val = np.mean(start_vals)  # estimated start of the support of mean pdf
     return qdf_to_pdf(mean_qdf, mean_start_val, centered_on_zero)
 
 
 def quantile_distance(
-        distr1: Function, distr2: Function, already_qf : bool = False
-    ) -> float:
+    distr1: Function, distr2: Function, already_qf: bool = False,
+) -> float:
     """Compute Wasserstein / Quantile distance for two given pdfs or qfs."""
     if already_qf:
         qf1, qf2 = distr1, distr2