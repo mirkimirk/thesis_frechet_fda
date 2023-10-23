ec4d5aeef2183912ca7853a147a8a6cb948a80cc
Mirkan Öcal <mirkanoecal@live.de>
2023-10-23 19:16:21 +0200

    Post Submission: started presentation tex file.

--- paper/task_paper.py
+++ paper/task_paper.py
@@ -5,7 +5,20 @@ import pytask
 from frechet_fda.config import BLD, PAPER_DIR
 from pytask_latex import compilation_steps as cs
 
-documents = ["frechet_fda"]
+documents = ["frechet_fda", "frechet_fda_pres"]
+
+DEPENDENCIES = {
+    "Chapter 1": PAPER_DIR / "Introduction.tex",
+    "Chapter 2": PAPER_DIR / "Maths.tex",
+    "Chapter 3": PAPER_DIR / "FDA.tex",
+    "Chapter 4": PAPER_DIR / "Densities.tex",
+    "Chapter 5": PAPER_DIR / "Fréchet.tex",
+    "Chapter 6": PAPER_DIR / "Application.tex",
+    "Chapter 7": PAPER_DIR / "Conclusion.tex",
+    "Appendix A": PAPER_DIR / "Numerics.tex",
+    "Appendix B": PAPER_DIR / "Code.tex",
+    "Appendix C": PAPER_DIR / "Figures.tex",
+},
 
 for document in documents:
 
@@ -16,22 +29,8 @@ for document in documents:
             options=("--pdf", "--interaction=nonstopmode", "--synctex=1", "--cd"),
         ),
     )
-    @pytask.mark.task(id=document)
-    @pytask.mark.depends_on(
-        {
-            "Chapter 1": PAPER_DIR / "Introduction.tex",
-            "Chapter 2": PAPER_DIR / "Maths.tex",
-            "Chapter 3": PAPER_DIR / "FDA.tex",
-            "Chapter 4": PAPER_DIR / "Densities.tex",
-            "Chapter 5": PAPER_DIR / "Fréchet.tex",
-            "Chapter 6": PAPER_DIR / "Application.tex",
-            "Chapter 7": PAPER_DIR / "Conclusion.tex",
-            "Appendix A": PAPER_DIR / "Numerics.tex",
-            "Appendix B": PAPER_DIR / "Code.tex",
-            "Appendix C": PAPER_DIR / "Figures.tex",
-        },
-    )
-    def task_compile_document():
+    @pytask.task(id=document)
+    def task_compile_document(paths = DEPENDENCIES):
         """Compile the document specified in the latex decorator."""
 
     kwargs = {
@@ -39,7 +38,7 @@ for document in documents:
         "produces": BLD.parent.resolve() / f"{document}.pdf",
     }
 
-    @pytask.mark.task(id=document, kwargs=kwargs)
+    @pytask.task(id=document, kwargs=kwargs)
     def task_copy_to_root(depends_on, produces):
         """Copy a document to the root directory for easier retrieval."""
         shutil.copy(depends_on, produces)
